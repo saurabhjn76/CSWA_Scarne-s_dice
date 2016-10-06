2a5566a8b54bcdbf95a27ae1cc9a445cc38a6461
saurabhjn76 <saurabhjn76@gmail.com>
2016-10-06 14:37:19 +0530

    Completed basic functionality.

--- app/src/main/java/saurabhjn76/com/scarnesdice/MainActivity.java
+++ app/src/main/java/saurabhjn76/com/scarnesdice/MainActivity.java
@@ -4,30 +4,130 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int userTotalScore=0;
+    private int userTurnScore=0;
+    private  int computerTotalScore=0;
+    private int computerTurnScore=0;
+    private Button roll,reset,hold;
+    private TextView userScore,computerScore;
+    private ImageView dieImage;
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        roll=(Button)findViewById(R.id.roll);
+        hold=(Button) findViewById(R.id.hold);
+        reset =(Button) findViewById(R.id.reset);
+        userScore =(TextView) findViewById(R.id.scoreUser);
+        computerScore =(TextView) findViewById(R.id.scoreComputer);
+        dieImage =(ImageView) findViewById(R.id.dieImage);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });*/
+        roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               rollDice(1);
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userTurnScore=0;
+                userTotalScore=0;
+                computerTurnScore=0;
+                computerTotalScore=0;
+                userScore.setText("User's Score: "+userTotalScore);
+                computerScore.setText("Computer's Score: "+computerTotalScore);
+
+            }
+        });
+        hold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holdScore(1);
+                computerTurn();
+
+            }
         });
     }
-
+    public void holdScore(int turn){
+        if(turn==1){
+            userTotalScore+=userTurnScore;
+            userTurnScore=0;
+            userScore.setText("User's Score: "+userTotalScore);
+        }
+        else{
+            computerTotalScore+=computerTurnScore;
+           computerTurnScore=0;
+            computerScore.setText("Computer's Score: "+computerTotalScore);
+        }
+    }
+    public  int rollDice(int turn){
+        int diceRoll= random.nextInt(6)+1;
+        int [] arr= {R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
+        dieImage.setImageDrawable(getResources().getDrawable(arr[diceRoll-1]));
+        if(diceRoll!=1){
+            if(turn==1) {
+                userTurnScore += diceRoll;
+                userScore.setText("User's Score: " + userTotalScore + " Your turnScore:" + userTurnScore);
+            }
+            else{
+                computerTurnScore += diceRoll;
+               computerScore.setText("Computer's Score: " + computerTotalScore + " Computer turnScore:" + computerTurnScore);
+            }
+        }
+        else{
+            if(turn==1) {
+                userTurnScore = 0;
+                holdScore(1);
+                computerTurn();
+                //userScore.setText("User's Score: " + userTotalScore + " Your turnScore:" + userTurnScore);
+            }
+            else{
+                computerTurnScore=0;
+                holdScore(2);
+               // computerScore.setText("Computer's Score: " + computerTotalScore + " Computer turnScore:" + computerTurnScore);
+            }
+        }
+    return diceRoll;
+    }
+    public void computerTurn(){
+        roll.setEnabled(false);
+        hold.setEnabled(false);
+        while(true){
+            if(rollDice(2)==1)
+                break;
+            if(computerTurnScore>20){
+                holdScore(2);
+                break;
+            }
+        }
+        roll.setEnabled(true);
+        hold.setEnabled(true);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
